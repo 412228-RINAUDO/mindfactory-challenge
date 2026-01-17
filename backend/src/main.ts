acd5817592b282cb1781bfdb4dc89e2203389edf
412228-RINAUDO <412228@tecnicatura.frc.utn.edu.ar>
2026-01-17 13:44:16 -0300

    add enable cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   
+  app.enableCors({
+    origin: 'http://localhost:5174',
+  });
+  
   app.setGlobalPrefix('api/v1');
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalPipes(
